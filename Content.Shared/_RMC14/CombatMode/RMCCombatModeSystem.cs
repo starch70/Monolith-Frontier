0fa5870214636b49ea20306da594c682df92859d
starch <starchpersonal@gmail.com>
2025-08-08 03:25:16 -0400

    RMC Wielded Crosshairs (REBALANCE BRANCH) (#1807)
    * initial
    
    * chore: automatically update REUSE headers
    
    ---------
    
    Co-authored-by: monolith8319 <195513600+monolith8319@users.noreply.github.com>

---
+++ Content.Shared/_RMC14/CombatMode/RMCCombatModeSystem.cs
@@ -0,0 +1,22 @@
+// SPDX-FileCopyrightText: 2025 starch
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using Content.Shared.Wieldable.Components;
+using Robust.Shared.Utility;
+
+namespace Content.Shared._RMC14.CombatMode;
+
+public sealed class RMCCombatModeSystem : EntitySystem
+{
+    public SpriteSpecifier.Rsi? GetCrosshair(Entity<WieldedCrosshairComponent?, WieldableComponent?> crosshair)
+    {
+        if (!Resolve(crosshair, ref crosshair.Comp1, ref crosshair.Comp2, false))
+            return null;
+
+        if (!crosshair.Comp2.Wielded)
+            return null;
+
+        return crosshair.Comp1.Rsi;
+    }
+}
---
+++ Content.Shared/_RMC14/CombatMode/WieldedCrosshairComponent.cs
@@ -0,0 +1,16 @@
+// SPDX-FileCopyrightText: 2025 starch
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using Robust.Shared.GameStates;
+using Robust.Shared.Utility;
+
+namespace Content.Shared._RMC14.CombatMode;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+[Access(typeof(RMCCombatModeSystem))]
+public sealed partial class WieldedCrosshairComponent : Component
+{
+    [DataField(required: true), AutoNetworkedField]
+    public SpriteSpecifier.Rsi? Rsi;
+}
