4de523dd72027d6039ba4a8aeecd13110962c0fe
monolith8319 <195513600+monolith8319@users.noreply.github.com>
2025-08-19 06:03:18 +0000

    chore: automatically update REUSE headers

--- Content.Server/Chemistry/Components/BaseSolutionInjectOnEventComponent.cs
+++ Content.Server/Chemistry/Components/BaseSolutionInjectOnEventComponent.cs
@@ -1,4 +1,9 @@
-﻿using Content.Shared.Damage; // Goobstation - Armor resisting syringe gun
+// SPDX-FileCopyrightText: 2024 Tayrtahn
+// SPDX-FileCopyrightText: 2025 starch
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using Content.Shared.Damage; // Goobstation - Armor resisting syringe gun
 using Content.Shared.FixedPoint;
 using Content.Shared.Inventory;
 
